f91e448376f1e46e873e9a630da5a0386c1ef793
vivian-wu <30644043+vivian-wu@users.noreply.github.com>
2017-11-22 01:06:24 +0800

    Updates AccountingActivity

--- app/src/main/java/imtbteam/imtb/AccountingActivity.java
+++ app/src/main/java/imtbteam/imtb/AccountingActivity.java
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class AccountingActivity extends AppCompatActivity {
 
     private EditText edAmount;
+    private RadioButton rev;
+    private  RadioButton exp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,11 @@ public class AccountingActivity extends AppCompatActivity {
 
         edAmount = (EditText) findViewById(R.id.ed_amount);
         RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup1);
+         exp = (RadioButton) findViewById(R.id.rbt_exp);
+         rev = (RadioButton) findViewById(R.id.rbt_rev);
+
+
+
     }
 
     public void add(View v){
