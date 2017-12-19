4e2b2b0d5016bbeecd16798d0bae4921a4190bb4
Icerainy <w70476@gmail.com>
2017-12-19 23:15:11 +0800

    Updates assets_record
    assets_record interface

--- app/src/main/java/imtbteam/imtb/assets_record.java
+++ app/src/main/java/imtbteam/imtb/assets_record.java
@@ -3,6 +3,7 @@ package imtbteam.imtb;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,10 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class assets_record extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private TextView textView6,textView8,textView9,textView10,textView11,textView12,textView13,textView14,textView15,textView16,textView17,textView18;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +33,19 @@ public class assets_record extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        final TextView textView6 = (TextView)findViewById(R.id.textView6);
+        final TextView textView8 = (TextView)findViewById(R.id.textView8);
+        final TextView textView9 = (TextView)findViewById(R.id.textView9);
+        final TextView textView10 = (TextView)findViewById(R.id.textView10);
+        final TextView textView11 = (TextView)findViewById(R.id.textView11);
+        final TextView textView12 = (TextView)findViewById(R.id.textView12);
+        final TextView textView13 = (TextView)findViewById(R.id.textView13);
+        final TextView textView14 = (TextView)findViewById(R.id.textView14);
+        final TextView textView15 = (TextView)findViewById(R.id.textView15);
+        final TextView textView16 = (TextView)findViewById(R.id.textView16);
+        final TextView textView17 = (TextView)findViewById(R.id.textView17);
+        final TextView textView18 = (TextView)findViewById(R.id.textView18);
+        textView18.setMovementMethod(ScrollingMovementMethod.getInstance());
 
     }
 
@@ -90,4 +105,6 @@ public class assets_record extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
