6d799d46a38d25707239b05f9d4cb6c25adebce4
vivian-wu <30644043+vivian-wu@users.noreply.github.com>
2017-12-29 01:13:58 +0800

    Updates Finance

--- app/src/main/java/imtbteam/imtb/FinanceActivity.java
+++ app/src/main/java/imtbteam/imtb/FinanceActivity.java
@@ -7,7 +7,6 @@ import android.database.sqlite.SQLiteDatabase;
 =======
 >>>>>>> Adds-ListView-to-FinanceActivity
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -31,6 +30,31 @@ public class FinanceActivity extends AppCompatActivity
     private MyDBHelper dbHelper;
     private TextView textView;
 =======
+import android.widget.ListView;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class FinanceActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SQLiteDatabase db;
+    private MyDBHelper dbHelper;
+
+    String[] cashid ;
+    String[] category;
+    String[] cardcontent;
+    String[] amount;
+    Adapter_ListViewActivity adapter;
+
+    // 宣告動態陣列
+    ArrayList<String> categoryList = new ArrayList<>();
+    ArrayList<String> contentList = new ArrayList<String>();
+    ArrayList<String> amountList = new ArrayList<String>();
+
 >>>>>>> Adds-ListView-to-FinanceActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +90,109 @@ public class FinanceActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-    }
 
+        ListView list = (ListView) findViewById(R.id.cash_listview);
+
+
+        dbHelper = new MyDBHelper(this); // 打開資料庫
+        db = dbHelper.getReadableDatabase();
+
+
+
+        //加入資料
+            // 查詢 SQL
+
+                 /*抓出最後一筆玩家ID*/
+
+                 String id = "";
+
+                 Cursor cursorid = db.rawQuery("Select PlayerID from PLAYER ORDER BY PlayerID desc limit 1 ", null);
+                 if (cursorid.getCount() != 0){
+                     do {
+                         cursorid.moveToLast();
+                         id = cursorid.getString(0);
+                         Log.d("現金記錄 - playerID:", id);
+                     } while (cursorid.moveToNext());
+                 }
+
+                 cursorid.close();
+
+                /*抓出他的現金資料*/
+                String query = "SELECT case  " +
+                "when t1.CardNo!='n'  then t2.CardCategory " +
+                "when t1.CardMarNo!='n' then t3.CardCategory " +
+                "when t1.CardOrderNo!='n' then t4.CardCategory " +
+                "when t1.CardInvestNo!='n' then t5.CardCategory " +
+                "when t1.CardOtherNo!='n' then t6.CardCategory " +
+                "END as Category, " +
+                "case  " +
+                "when t1.CardNo!='n'  then t2.CardContent " +
+                "when t1.CardMarNo!='n' then t3.CardContent " +
+                "when t1.CardOrderNo!='n' then '獲得訂單' " +
+                "when t1.CardInvestNo!='n' AND t1.Amount<0 then '買進股票' " +
+                "when t1.CardInvestNo!='n' AND t1.Amount>0 then '賣出股票' " +
+                "when t1.CardOtherNo!='n' then t6.CardContent " +
+                "END as Content " +
+                ",t1.Amount " +
+                "from CASHFLOW t1 " +
+                "LEFT JOIN Card t2 on t1.CardNo=t2.CardNo " +
+                "LEFT JOIN CARD_MARKET t3 on t1.CardMarNo=t3.CardMarNo " +
+                "LEFT JOIN CARD_ORDER t4 on t1.CardOrderNo=t4.CardOrderNo " +
+                "LEFT JOIN CARD_INVESTMENT t5 on t1.CardInvestNo=t5.CardInvestNo " +
+                "LEFT JOIN CARD_OTHER t6 on t1.CardOtherNo=t6.CardOtherNo " +
+                "WHERE t1.PlayerID='"+id+"'";
+
+                Cursor cursorinfo = db.rawQuery(query, null);
+                int arraylength = 0;
+
+                if (cursorinfo.getCount() != 0){
+                    cursorinfo.moveToFirst();
+                    do {
+                            categoryList.add(cursorinfo.getString(0));    //卡片類別
+                            contentList.add(cursorinfo.getString(1));     //卡片內容
+                            amountList.add(cursorinfo.getString(2));  //金額
+                            Log.d("現金紀錄","category:"+cursorinfo.getString(0)+"content:"+cursorinfo.getString(1)+"amount"+cursorinfo.getString(2));
+
+                    } while (cursorinfo.moveToNext());
+
+                    arraylength = cursorinfo.getCount();
+                }
+                else
+                {
+                    Log.d("現金紀錄","失敗");
+                }
+
+
+                cursorinfo.close();
+
+                cashid = new String[arraylength];
+
+                for (int i=0; i < arraylength ; i++)
+                {
+                    cashid[i] = String.valueOf(i+1);
+                }
+
+                // 動態陣列轉換成一般陣列(to array)
+                Object ca1[] = categoryList.toArray();
+                category = Arrays.copyOf(ca1, ca1.length, String[].class);
+
+                Object co2[] = contentList.toArray();
+                cardcontent = Arrays.copyOf(co2, co2.length, String[].class);
+
+                Object am3[] = amountList.toArray();
+                amount = Arrays.copyOf(am3, am3.length, String[].class);
+                System.out.print("陣列筆數:"+ca1.length);
+
+
+
+        adapter = new Adapter_ListViewActivity(FinanceActivity.this,
+                R.layout.finance_row,
+                cashid,
+                category,
+                cardcontent,amount);
+
+        list.setAdapter(adapter);
+    }
 
 
     @Override
